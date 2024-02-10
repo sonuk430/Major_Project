5808ce24bf85686873967d5c997da1c9a322915e
Sonu Kumar <kumarsonuk430@gmail.com>
2024-02-11 01:24:30 +0530

    logic done

--- Typing_Speed_Test/app.js
+++ Typing_Speed_Test/app.js
@@ -1,15 +1,110 @@
 const typingTextEl = document.querySelector(".typing-text p");
 
-// find random paragraphs;
+const inputDataEl = document.querySelector("main .inputData");
 
+let timeEl = document.querySelector(".time span b");
+
+let mistakesEL = document.querySelector(".mistakes span");
+let wpmEl = document.querySelector(".wpm span");
+let cpmEl = document.querySelector(".cpm span");
+
+const tryAgainBtn = document.querySelector(".tryAgain");
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+
+let charIndex = (mistakes = isTyping = 0);
+
+// find random paragraphs;
 function randomParagraph() {
   let randomPera = Math.floor(Math.random() * paragraphs.length);
-
+  typingTextEl.innerHTML = "";
   // random pera hm apne array se le rahe he or us pera ke sare corector ko split kr typingTextEl "p" tag me store kr rahe he.
   paragraphs[randomPera].split("").forEach((span) => {
     let spanTag = `<span>${span}</span>`;
     typingTextEl.innerHTML += spanTag;
   });
+
+  typingTextEl.querySelectorAll("span")[0].classList.add("active");
+  // work on the input field
+  document.addEventListener("keydown", () => {
+    inputDataEl.focus();
+  });
+
+  typingTextEl.addEventListener("click", () => {
+    inputDataEl.focus();
+  });
+}
+
+function typing() {
+  const charaters = typingTextEl.querySelectorAll("span");
+  // teken all input text value;
+  let typeChar = inputDataEl.value.split("")[charIndex];
+
+  if (charIndex < charaters.length - 1 && timeLeft > 0) {
+    if (!isTyping) {
+      timer = setInterval(initTimer, 1000);
+      isTyping = true;
+    }
+
+    if (typeChar == null) {
+      charIndex--;
+      if (charaters[charIndex].classList.contains("incorrect")) {
+        mistakes--;
+      }
+      charaters[charIndex].classList.remove("correct", "incorrect");
+    } else {
+      if (charaters[charIndex].innerText === typeChar) {
+        charaters[charIndex].classList.add("correct");
+        // console.log("correct");
+      } else {
+        mistakes++;
+        charaters[charIndex].classList.add("incorrect");
+        // console.log("in correct");
+      }
+      charIndex++;
+    }
+
+    charaters.forEach((span) => span.classList.remove("active"));
+    charaters[charIndex].classList.add("active");
+    // console.log(typeChar);
+
+    let wpm = Math.round(
+      ((charIndex - mistakes) / 5 / (maxTime - timeLeft)) * 60
+    );
+
+    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+    mistakesEL.innerText = mistakes;
+    wpmEl.innerText = wpm;
+    cpmEl.innerText = charIndex - mistakes;
+  } else {
+    inputDataEl.value = "";
+    clearInterval(timer);
+  }
+}
+
+function initTimer() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    timeEl.innerText = timeLeft;
+  } else {
+    clearInterval(timer);
+  }
+}
+
+function reset() {
+  randomParagraph();
+  inputDataEl.value = "";
+  clearInterval(timer);
+  timeLeft = maxTime;
+  charIndex = mistakes = isTyping = 0;
+  timeEl.innerText = timeLeft;
+  mistakesEL.innerText = 0;
+  cpmEl.innerText = 0;
 }
 
 randomParagraph();
+
+inputDataEl.addEventListener("input", typing);
+tryAgainBtn.addEventListener("click", reset);
--- Typing_Speed_Test/paragraphs.js
+++ Typing_Speed_Test/paragraphs.js
@@ -1,6 +1,13 @@
 const paragraphs = [
-  "i am sonu kumar",
-  "i am from bihar",
-  "i am mechanical eng.",
-  "i am frontEnd developer",
+  "You have the right to perform your actions, but you are not entitled to the fruits of your actions.",
+  "The soul is neither born, nor does it ever die; nor having once existed, does it ever cease to be. The soul is without birth, eternal, immortal, and ageless. It is not destroyed when the body is destroyed.",
+  "Perform your obligatory duty, because action is indeed better than inaction.",
+  "One who sees inaction in action, and action in inaction, is intelligent among men, and he is in the transcendental position, although engaged in all sorts of activities.",
+  "He who is the same to friend and foe, and also in honor and dishonor, who is the same in cold and heat, pleasure and pain, who is free from attachment, to whom censure and praise are equal, who is silent, content with anything, homeless, of a steady mind, and full of devotion—that one is dear to Me",
+  "Whatever happened, happened for the good; whatever is happening, is happening for the good; whatever will happen, will also happen for the good only. You need not have any regrets for the past. You need not worry for the future. The present is happening",
+  "Perform all thy actions with mind concentrated on the Divine, renouncing attachment and looking upon success and failure with an equal eye. Spirituality implies equanimity",
+  "A person can rise through the efforts of his own mind; or draw himself down, in the same manner. Because each person is his own friend or enemy.",
+  "You have control over doing your respective duty only, but no control or claim over the results. The fruits of work should not be your motive; you should never be inactive.",
+  "Those who possess this wisdom have equal regard for all. They see the same Self in a spiritual aspirant and an outcaste, in an elephant, a cow, and a dog.",
+  "The happiness which comes from long practice, which leads to the end of suffering, which at first is like poison, but at last like nectar—this kind of happiness arises from the serenity of one's own mind.",
 ];
